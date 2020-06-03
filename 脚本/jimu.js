60ae9f4c59b0b8225d8e1c685b133f384cb4ef70
xiong233 <31365243+xiong233@users.noreply.github.com>
2020-06-04 00:04:19 +0800

    Update jimu.js

--- 脚本/jimu.js
+++ 脚本/jimu.js
@@ -1,22 +1,29 @@
 var body = $response.body;
 var url = $request.url;
 
-const path1 = 'api/v1/sell/user/privilege';
+const path1 = '/api/v1/sell/user/privilege';
+
+const path2 = '/time/getconfig';
 
 let obj = JSON.parse(body);
 
 if (url.indexOf(path1) != -1) {
-	obj.user_subscription['data']['privileges']['likeme']['status'] = 1;
-    obj.user_subscription['data']['privileges']['vip']['status'] = 1;
-	obj.user_subscription['data']['privileges']['likeme']['buy_status'] = 1;
-	obj.user_subscription['data']['privileges']['vip']['buy_status'] = 1;
-	obj.user_subscription['data']['privileges']['likeme']['rest_days'] = 1;
-	obj.user_subscription['data']['privileges']['vip']['rest_days'] = 1;
-	obj.user_subscription['data']['privileges']['likeme']['expire_time_desc'] = '2021-01-01 08:00:00';
-	obj.user_subscription['data']['privileges']['vip']['expire_time_desc'] = '2021-01-01 08:00:00';
-	obj.user_subscription['data']['privileges']['likeme']['end_time'] = '2021-01-01 08:00:00';
-	obj.user_subscription['data']['privileges']['vip']['end_time'] = '2021-01-01 08:00:00';
+	obj.data['privileges']['likeme']['status'] = 1;
+    obj['data']['privileges']['vip']['status'] = 1;
+	obj.data['privileges']['likeme']['buy_status'] = 1;
+	obj['data']['privileges']['vip']['buy_status'] = 1;
+	obj['data']['privileges']['likeme']['rest_days'] = 1;
+	obj['data']['privileges']['vip']['rest_days'] = 1;
+	obj['data']['privileges']['likeme']['expire_time_desc'] = '2021-01-01 08:00:00';
+	obj['data']['privileges']['vip']['expire_time_desc'] = '2021-01-01 08:00:00';
+	obj['data']['privileges']['likeme']['end_time'] = '2021-01-01 08:00:00';
+	obj['data']['privileges']['vip']['end_time'] = '2021-01-01 08:00:00';
 	body = JSON.stringify(obj);  
  }
+
+if (url.indexOf(path2) != -1) {
+	obj=null;
+    body = JSON.stringify(obj);
+}
 
 $done({body});
