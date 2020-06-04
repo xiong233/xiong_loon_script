03c050d54227c3713a66af7a63ac3b4b3297790e
xiong233 <31365243+xiong233@users.noreply.github.com>
2020-06-05 00:23:37 +0800

    Update xmly.js

--- 脚本/xmly.js
+++ 脚本/xmly.js
@@ -6,7 +6,7 @@ const path1 = 'ximalaya.com/wechat/api/open/openPay/albumBoughtStatus';
 let obj = JSON.parse(body);
 
 if (url.indexOf(path1) != -1) {
-	obj['data']['']['boughtStatus'] = 'True';
+	obj['data'][0]['boughtStatus'] = 'True';
 	body = JSON.stringify(obj);  
  }
 
