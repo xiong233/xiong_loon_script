af76e5e06352ebe4a0cc6c010523dc148cd1d5ab
xiong233 <31365243+xiong233@users.noreply.github.com>
2020-06-10 14:27:12 +0800

    Update now.js

--- 脚本/now.js
+++ 脚本/now.js
@@ -1,19 +1,20 @@
-var body = $response.body;
 var url = $request.url;
 
 const path1 = 'nowapi.navoinfo.cn/my_vip';
 const path2 = 'nowapi.navoinfo.cn/user?get_pendant=1&pub_device_id=';
 const path3 = 'nowapi.navoinfo.cn/user_center?pub_device_id=';
 
-let obj = JSON.parse(body);
-
 if (url.indexOf(path1) != -1) {
+	var body = $response.body;
+	let obj = JSON.parse(body);
 	obj['result']['user_info']['vip_forever'] = 'true';
-	body = JSON.stringify(obj);  
+	body = JSON.stringify(obj);
+	$done({body});
  }
- if ((url.indexOf(path2) != -1)||(url.indexOf(path2) != -1)) {
+ if ((url.indexOf(path2) != -1)||(url.indexOf(path3) != -1)) {
+	var body = $response.body;
+	let obj = JSON.parse(body);
 	obj['result']['vip_forever'] = 'true';
-	body = JSON.stringify(obj);  
+	body = JSON.stringify(obj);
+	$done({body});
  }
-
-$done({body});
