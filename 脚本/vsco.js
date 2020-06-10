1a6406483b1ada98078109686fc24d6657f8cea5
xiong233 <31365243+xiong233@users.noreply.github.com>
2020-06-10 14:33:00 +0800

    Update vsco.js

--- 脚本/vsco.js
+++ 脚本/vsco.js
@@ -1,15 +1,13 @@
-var body = $response.body;
 var url = $request.url;
 
-const path1 = '/api/subscriptions/2.1/user-subscriptions/';
-
-let obj = JSON.parse(body);
+const path1 = 'vsco.co/api/subscriptions/2.1/user-subscriptions/';
 
 if (url.indexOf(path1) != -1) {
+	var body = $response.body;
+	let obj = JSON.parse(body);
 	
 	obj.user_subscription["is_active"] = true;
 
 	body = JSON.stringify(obj);  
+	$done({body});
  }
-
-$done({body});
