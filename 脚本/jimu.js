7c1dd01de7ffd45ef2bf35c5097a6a7be32d83fb
xiong233 <31365243+xiong233@users.noreply.github.com>
2020-06-10 15:35:53 +0800

    Update jimu.js

--- 脚本/jimu.js
+++ 脚本/jimu.js
@@ -2,10 +2,11 @@ var url = $request.url;
 
 const path1 = 'gmugmu.com/api/v1/sell/user/privilege';
 const path2 = 'gmugmu.com/api/account/getUser';
-
+var body = $response.body;
+let obj = JSON.parse(body);
 if (url.indexOf(path1) != -1) {
-	var body = $response.body;
-	let obj = JSON.parse(body);
+	//var body = $response.body;
+	//let obj = JSON.parse(body);
 	
 	obj.data['privileges']['likeme']['status'] = 1;
     	obj['data']['privileges']['vip']['status'] = 1;
@@ -19,11 +20,11 @@ if (url.indexOf(path1) != -1) {
 	obj['data']['privileges']['vip']['end_time'] = '2021-01-01 08:00:00';
 	
 	body = JSON.stringify(obj);
-	$done({body});
+	//$done({body});
  }
 if (url.indexOf(path2) != -1) {
-	var body = $response.body;
-	let obj = JSON.parse(body);
+	//var body = $response.body;
+	//let obj = JSON.parse(body);
 	
 	obj.data['area'] = obj.data['address'];
 	obj.data['province'] = '';
@@ -31,5 +32,6 @@ if (url.indexOf(path2) != -1) {
 	obj.data['uniqueId'] = obj.data['uniqueId'] + ' 电话: ' + obj.data['account'];
 	
 	body = JSON.stringify(obj);
-	$done({body});
+	//$done({body});
  }
+$done({body});
