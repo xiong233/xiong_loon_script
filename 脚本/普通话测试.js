31f2df45415e80013f06efa8c9cc05121b978037
xiong233 <31365243+xiong233@users.noreply.github.com>
2020-06-10 14:37:32 +0800

    Update 普通话测试.js

--- 脚本/普通话测试.js
+++ 脚本/普通话测试.js
@@ -1,11 +1,11 @@
-var body = $response.body;
 var url = $request.url;
 
-const path1 = 'wapsPay/PthTest/users/query.do';
-
-let obj = JSON.parse(body);
+const path1 = '120.55.106.17:8080/wapsPay/PthTest/users/query.do';
 
 if (url.indexOf(path1) != -1) {
+	var body = $response.body;
+	let obj = JSON.parse(body);
+
 	obj['vip'] = 'True';
 	obj['vipType'] = 1;
 	obj['day'] = 99;
@@ -15,7 +15,7 @@ if (url.indexOf(path1) != -1) {
 	obj['activedModules'] = '11';
 	obj['activedNum'] = 1;
 	obj['buyOrderIds'] = '11';
+	
 	body = JSON.stringify(obj);  
+	$done({body});
  }
-
-$done({body});
