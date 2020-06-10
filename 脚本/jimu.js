b76d6b3c4e21c12bdb469cd0ad42b6b2b36dc4a7
xiong233 <31365243+xiong233@users.noreply.github.com>
2020-06-10 23:57:55 +0800

    Update jimu.js

--- 脚本/jimu.js
+++ 脚本/jimu.js
@@ -2,6 +2,8 @@ var url = $request.url;
 
 const path1 = 'gmugmu.com/api/v1/sell/user/privilege';
 const path2 = 'gmugmu.com/api/account/getUser';
+const path3 = 'gmugmu.com/api/account/v2/social/likeme/recent';
+
 var body = $response.body;
 let obj = JSON.parse(body);
 if (url.indexOf(path1) != -1) {
@@ -26,7 +28,7 @@ if (url.indexOf(path2) != -1) {
 	//var body = $response.body;
 	//let obj = JSON.parse(body);
 	
-	//obj.data['area'] = obj.data['address'];
+	obj.data['area'] = obj.data['address'];
 	obj.data['province'] = '';
 	obj.data['city'] = '';
 	obj.data['uniqueId'] = obj.data['uniqueId'] + ' 电话: ' + obj.data['account'];
@@ -34,4 +36,20 @@ if (url.indexOf(path2) != -1) {
 	body = JSON.stringify(obj);
 	//$done({body});
  }
+ 
+ if (url.indexOf(path3) != -1) {
+	for (var key in obj['data']['data_map'])
+	{
+		var accountslen = obj['data']['data_map'][key]['accounts'].length;
+		for (var i = 0; i < accountslen; i++)
+		{
+			var id = obj['data']['data_map'][key]['accounts'][i]['account']['avatar'].split('/')[3].split('_')[0];
+			obj['data']['data_map'][key]['accounts'][i]['account']['id']=id;
+		}
+				
+	}
+	
+	body = JSON.stringify(obj);
+ }
+ 
 $done({body});
