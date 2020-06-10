32555a0ccc85b98a0dbf133ff7a8e9597f3c2d9a
xiong233 <31365243+xiong233@users.noreply.github.com>
2020-06-10 14:34:39 +0800

    Update yunting.js

--- 脚本/yunting.js
+++ 脚本/yunting.js
@@ -1,13 +1,13 @@
-var body = $response.body;
 var url = $request.url;
 
 const path1 = 'getway.radio.cn/app';
 
-let obj = JSON.parse(body);
-
 if (url.indexOf(path1) != -1) {
+	var body = $response.body;
+	let obj = JSON.parse(body);
+	
 	obj['isVip'] = 0;
+	
 	body = JSON.stringify(obj);  
+	$done({body});
  }
-
-$done({body});
